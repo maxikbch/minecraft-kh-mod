9bb7833065b2cbc0b9ed9590feb15bd0262aea3d
Maxi Kbch <maxikallenbach260@gmail.com>
2024-08-30 21:00:25 -0300

    base commit

---
+++ src/main/java/net/maxo13/maxoskhmod/KingdomHeartsModClient.java
@@ -0,0 +1,10 @@
+package net.maxo13.maxoskhmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KingdomHeartsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
